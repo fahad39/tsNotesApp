4ff6e4d53ad2c6fcd3b06d2924e3888cd493d361
fahad39 <fahadhussain0127@gmail.com>
2023-10-08 14:50:03 +0500

    added eslint

--- server/dist/server.js
+++ server/dist/server.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 5000;
 app.get("/", (req, res) => {
-    res.send("Hello world");
+    res.send("Hello, world");
 });
 app.listen(port, () => {
     console.log("server is running: ", port);
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Hello, world");
 });
 
 app.listen(port, () => {
