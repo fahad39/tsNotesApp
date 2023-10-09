bbb96b8b8d36254606c11732f958b181481375f0
fahad39 <fahadhussain0127@gmail.com>
2023-10-09 15:07:11 +0500

    added server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
   res.send("Hello, world");
 });
 
-app.listen(PORT, () => {
-  console.log("server is running: ", PORT);
-});
+mongoose
+  .connect(process.env.DATABASE_URL!)
+  .then(() => {
+    console.log("Mongoose connected");
+    app.listen(PORT, () => {
+      console.log("server is running: ", PORT);
+    });
+  })
+  .catch(console.error);
