dfa99cca877f0b9814db0131248743a0d8674fc3
fahad39 <fahadhussain0127@gmail.com>
2023-10-12 10:18:48 +0500

    added better error handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import notesRoutes from "./routes/notes";
+import createHttpError, { isHttpError } from "http-errors";
 import morgan = require("morgan");
 
 dotenv.config();
@@ -13,14 +14,18 @@ app.use(express.json());
 app.use("/api/notes", notesRoutes);
 
 app.use((req, res, next) => {
-  next(Error("Endpoint not found"));
+  next(createHttpError(404, "Endpoint not found"));
 });
 
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
   let errorMessage = "An unknown error occurred";
-  if (error instanceof Error) errorMessage = error.message;
-  res.status(500).json({ error: errorMessage });
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({ error: errorMessage });
 });
 
 export default app;
