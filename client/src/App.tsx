4c235462a761a771a84a744d97a3c3a65e2a37eb
fahad39 <fahadhussain0127@gmail.com>
2023-10-19 20:02:00 +0500

    added create note api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import { Note as NoteModel } from "./models/note";
 import Note from "./components/Note";
 import { Col, Container, Row } from "react-bootstrap";
 import styles from "../src/styles/NotesPage.module.css";
+import * as NoteApi from "./network/notes_api";
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
@@ -11,10 +11,7 @@ function App() {
   useEffect(() => {
     async function loadNotes() {
       try {
-        const response = await fetch("/api/notes", {
-          method: "GET",
-        });
-        const notes = await response.json();
+        const notes = await NoteApi.fetchNotes();
         setNotes(notes);
       } catch (error) {
         console.error(error);
@@ -27,7 +24,7 @@ function App() {
   return (
     <Container>
       <Row xs={1} md={2} xl={3} className="g-4">
-        {notes.map((item, index) => {
+        {notes?.map((item, index) => {
           return (
             <Col key={item._id}>
               <Note note={item} style={styles.note} />
