da0463f571b0b957d9443a916f3e2e31fae871cf
fahad39 <fahadhussain0127@gmail.com>
2023-10-19 19:40:57 +0500

    added time util function

--- client/src/components/Note.tsx
+++ client/src/components/Note.tsx
@@ -2,6 +2,7 @@ import styles from "../styles/Note.module.css";
 import React from "react";
 import { Note as NoteModel } from "../models/note";
 import { Card } from "react-bootstrap";
+import { formatDate } from "../utils/formatDate";
 
 interface NoteProps {
   note: NoteModel;
@@ -9,13 +10,21 @@ interface NoteProps {
 }
 const Note = ({ note, style }: NoteProps) => {
   const { title, text, createdAt, updatedAt } = note;
+
+  let createdUpdatedText: string;
+  if (updatedAt > createdAt) {
+    createdUpdatedText = "Updated At: " + formatDate(updatedAt);
+  } else {
+    createdUpdatedText = "Created At: " + formatDate(createdAt);
+  }
+
   return (
     <Card className={`${styles.noteCard} ${style}`}>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text className={styles.cardText}>{text}</Card.Text>
       </Card.Body>
-      <Card.Footer className="text=muted">{createdAt}</Card.Footer>
+      <Card.Footer className="text=muted">{createdUpdatedText}</Card.Footer>
     </Card>
   );
 };
